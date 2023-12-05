30a1905375f6844322333222da20a9a10f062ab1
Igor <isabanchin@gmail.com>
2023-12-05 20:18:36 +0300

    Added exersize pages

--- project/manage.py
+++ project/manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+# admin: 1234@qwe
+
 
 def main():
     """Run administrative tasks."""
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('/', ur.view.here),
     path('admin/', admin.site.urls),
+    path('pages/', include("django.contrib.flatpages.urls")),
+    # Скорее всего, лишние пути:
+    path('templates/', include("django.contrib.flatpages.urls")),
+    path('templates/flatpages', include("django.contrib.flatpages.urls")),
 ]
